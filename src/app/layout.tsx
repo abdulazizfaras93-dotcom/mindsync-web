80c94b97cfd29fca7b419fb21fa6489093013cfb
Abdulaziz Faras <abdulazizfaras93@gmail.com>
2026-04-28 04:09:13 +0300

    chore: use logo-transparent.png as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,8 +74,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressHydrationWarning
     >
       <head>
-        <link rel="icon" href="/brand/logo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/brand/logo.png" />
+        <link rel="icon" href="/brand/logo-transparent.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/brand/logo-transparent.png" />
       </head>
       <body className="antialiased">{children}</body>
     </html>
